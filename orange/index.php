de4d48c147af466267052b93a2e0efa66352f848
Andy Knudsen <paris_giovanni@yahoo.com>
2018-12-27 11:23:11 -0700

    switched buttons

--- orange/index.php
+++ orange/index.php
@@ -33,7 +33,7 @@
     $bgColor = "#7ECDF4";
     $btnColor = "#3D9CEB";
 	$alt = "Whiteboard Animation Demo";
-    include("../../ivideo/showVideo.php"); 
+    include("../includes/showVideo.php"); 
     ?>
 			</div>
 			<div class="col-xl-3">
@@ -57,7 +57,7 @@
     $show = "15";
 	$rand = false;
 	$columns = 3;
-    include("../../ivideo/showDemo.php"); 
+    include("../includes/showDemoBtn.php"); 
     ?>
 		</div>
 </section>
@@ -73,7 +73,7 @@
     $show = "6";
 	$rand = true;
 	$columns = 3;
-    include("../../ivideo/showDemo.php"); 
+    include("../includes/showDemoBtn.php");
     ?>
 		</div>
 	</div>
