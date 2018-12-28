d737af44dbfdeab4ef3acfa83b2d66280db5d7b9
Andy Knudsen <andy@websitetalkingheads.com>
2018-12-28 13:27:31 -0700

    Fixed sizing for testimonials on all versions

--- includes/testimonial-awards.php
+++ includes/testimonial-awards.php
@@ -8,7 +8,7 @@
 				<a href="#" data-toggle="tooltip" title="Best in Video SEO"> <img class="img img-fluid seal" src="https://www.websitetalkingheads.com/images/video-seo-seal.png"  alt="Best in Video SEO" title="Best in Video SEO"	 /></a>
 			</div>
 		</div>
-		<div class="col-md-8" style="height: 191px">
+		<div class="col-md-8 testimonial-holder">
 			<h2 class="text-center">Why are we the #1 Video Production Company</h2>
 			<h3 class="client">Client Testimonials</h2>
 			<div class="testimonials">
